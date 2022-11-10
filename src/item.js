d6f75734be511ca3b4c8517a13ad00e76e4f3d45
Eva <skyicelands@gmail.com>
2022-11-10 15:27:58 +0300

    stage 4

--- src/item.js
+++ src/item.js
@@ -1,14 +1,14 @@
 export function drawGalleryItem(item) {
     const itemEl = document.createElement('div')
-    itemEl.classList = "gallery-item"
+    itemEl.classList = "gallery__item"
 
 
     const imgEl = document.createElement("img")
-    imgEl.classList = "gallery-item__image"
+    imgEl.classList = "gallery__item__image"
     imgEl.src = item.resource
 
     const titleEl = document.createElement('span')
-    titleEl.classList = "gallery-item__title"
+    titleEl.classList = "gallery__item__title"
     titleEl.textContent = item.title
 
     itemEl.appendChild(imgEl)
